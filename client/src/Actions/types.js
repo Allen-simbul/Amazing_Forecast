1db07d5b16ef2fbf3fd28926007b98c0f611064d
allen <allen.simbul591@gmail.com>
2020-06-05 16:31:45 +0100

    fixed issue with not finding result from search term

---
+++ client/src/Actions/types.js
@@ -0,0 +1,2 @@
+export const FETCH_FORECAST = 'FETCH_FORECAST';
+export const CITY_NOT_FOUND = 'CITY_NON_FOUND';
--- client/src/Reducers/forecast-reducer.js
+++ client/src/Reducers/forecast-reducer.js
@@ -1,6 +1,8 @@
+import { FETCH_FORECAST } from '../Actions/types';
+
 export default function (state = {}, action) {
   switch (action.type) {
-    case 'FETCH_FORECAST':
+    case FETCH_FORECAST:
       return action.payload;
     default:
       return state;
