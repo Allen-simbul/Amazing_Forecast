b108dd924c47d76216b0e2c75cd882b436f55e01
allen <allen.simbul591@gmail.com>
2020-06-11 15:55:42 +0100

    render data from server side data

--- client/src/Components/Forecast.js
+++ client/src/Components/Forecast.js
@@ -1,9 +1,48 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 class Forecast extends React.Component {
+  renderMainForecast = () => {
+    return (
+      <div className="main_forecast_container">
+        <div className="main_forecast_left">
+          <div className="main_forecast_city_name">
+            {this.props.currentForecast.cityName}
+          </div>
+          <div className="main_forecast_current_time">
+            {this.props.currentForecast.currentTime}
+          </div>
+          <div className="main_forecast_temperature">
+            {this.props.currentForecast.main.temp}
+          </div>
+          <div className="main_forecast_weather_description">
+            {this.props.currentForecast.weather.description}
+          </div>
+          <div className="main_forecast_weather_main">
+            {this.props.currentForecast.weather.main}
+          </div>
+        </div>
+
+        <div className="main_forecast_right">
+          <i>Icon</i>
+        </div>
+      </div>
+    );
+  };
   render() {
-    return <div>Forecast</div>;
+    return (
+      <div>
+        Forecast
+        {this.renderMainForecast()}
+      </div>
+    );
   }
 }
 
-export default Forecast;
+const mapStateToProps = (state) => {
+  return {
+    currentForecast: state.currentForecast,
+  };
+};
+
+export default connect(mapStateToProps)(Forecast);
--- routes/forecast-route.js
+++ routes/forecast-route.js
@@ -5,9 +5,32 @@ require('../db/mongoose');
 const Location = require('../models/Location');
 const Code = require('../models/Code');
 
+const getWeatherData = async (city) => {
+  const weatherData = {};
+  const response = await openweather(city);
+  weatherData.cityName = response.name;
+  weatherData.cod = response.cod;
+  weatherData.coord = response.coord;
+  weatherData.clouds = response.clouds;
+  weatherData.currentTime = response.dt;
+  weatherData.locationID = response.id;
+  weatherData.main = response.main;
+  weatherData.rain = response.rain;
+  weatherData.sun = {
+    sunriseTime: response.sys.sunrise,
+    sunsetTime: response.sys.sunset,
+  };
+  weatherData.weather = {
+    description: response.weather[0].description,
+    main: response.weather[0].main,
+  };
+  weatherData.wind = response.wind;
+  return weatherData;
+};
+
 router.get('/api/forecast/:city', async (req, res) => {
-  const response = await openweather(req.params.city);
-  // console.log(response.cod === 404);
+  const response = await getWeatherData(req.params.city);
+  console.log(response);
   res.send(response);
 });
 
